4b0e8e5d4fa7beceb9f37e6021bcfefbc25aeed0
divya anusha <anuchandrupatla13@gmail.com>
2020-02-10 06:46:46 +0530

    first commit

---
+++ src/main/java/oop_principles/hometask_epam/App.java
@@ -0,0 +1,56 @@
+package oop_principles.hometask_epam;
+import java.util.*;
+interface Calculate
+{
+	public int process_numbers(int a,int b);
+}
+class Add implements Calculate
+{
+	public int process_numbers(int a,int b)
+	{
+		return (a+b);
+	}
+	
+}
+class Subtract implements Calculate
+{
+	public int process_numbers(int a,int b)
+	{
+		return (a-b);
+	}
+}
+class Multiply implements Calculate
+{
+	public int process_numbers(int a,int b)
+	{
+		return (a*b);
+	}
+}
+class Divide implements Calculate
+{
+	public int process_numbers(int a,int b)
+	{
+		return (a/b);
+	}
+}
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Scanner sc= new Scanner(System.in);
+        Calculate c;
+        System.out.println("enter first element : ");
+        int num_1=sc.nextInt();
+        System.out.println("enter second element : ");
+        int num_2=sc.nextInt();
+        c=new Add();
+        System.out.println("after addition between "+num_1+" and "+ num_2+ " : "+c.process_numbers(num_1, num_2));
+        c=new Subtract();
+        System.out.println("after subtraction between "+num_1+" and "+ num_2+ " : "+c.process_numbers(num_1, num_2));
+        c=new Multiply();
+        System.out.println("after multiplication between "+num_1+" and "+ num_2+ " : "+c.process_numbers(num_1, num_2));
+        c=new Divide();
+        System.out.println("after dividion between "+num_1+" and "+ num_2+ " : "+c.process_numbers(num_1, num_2));
+        sc.close();
+    }
+}
